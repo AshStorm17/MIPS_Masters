736289e071fa2a293c067b533c788a9761e3980e
Aashmun Gupta <aashmun.gupta@iitgn.ac.in>
2024-09-19 23:04:23 +0530

    I inst added

--- execute.py
+++ execute.py
@@ -1,3 +1,16 @@
+def signedVal(binStr):
+    isSigned = int(binStr[0]=="1")
+    return int(binStr, 2) - isSigned*(2**len(binStr))
+
+def signedBin(num):
+    # return 2's complement binary string of length len
+    ans=""
+    if(num<0):
+        ans = bin(num % (1<<32))[2:]
+    else:
+        ans = format(num, '032b')
+    return ans
+
 class Execute:
     def __init__(self, memory, registers, alu):
         self.memory = memory
@@ -24,8 +37,6 @@ class Execute:
                     opr1 = self.registers.reg[inst.rs]
                     opr2 = self.registers.reg[inst.rt]
                     ans = self.alu.alu_arith(inst.funct, opr1, opr2)
-
-                pass
             case 2|3:
                 # handle J-type inst 
                 """
@@ -45,6 +56,42 @@ class Execute:
                     lui, 
                     ori, andi,
                 """
+                match inst.op:
+                    case 0b001000:
+                        # addi
+                        src = self.registers.reg[inst.rs]
+                        ans = src + inst.immediate
+                        self.registers.reg[inst.rt] = ans
+                    case 0b001010:
+                        # slti
+                        src = self.registers.reg[inst.rs]
+                        imm = signedVal(inst.immediate)
+                        ans = 1 if src < imm else 0
+                        self.registers.reg[inst.rt] = ans
+                    case 0b001011:
+                        # sltiu
+                        src = self.registers.reg[inst.rs]
+                        ans = 1 if src < inst.immediate else 0
+                        self.registers.reg[inst.rt] = ans
+                    case 0b001100:
+                        # andi
+                        src = self.registers.reg[inst.rs]
+                        ans = src & inst.immediate
+                        self.registers.reg[inst.rt] = ans
+                    case 0b001101:
+                        # ori
+                        src = self.registers.reg[inst.rs]
+                        ans = src | inst.immediate
+                        self.registers.reg[inst.rt] = ans
+                    case 0b001110:
+                        # xori
+                        src = self.registers.reg[inst.rs]
+                        ans = src ^ inst.immediate
+                        self.registers.reg[inst.rt] = ans
+                    case 0b001111:
+                        # lui
+                        ans = inst.immediate << 16
+                        self.registers.reg[inst.rt] = ans
                 pass
 
 # -------------------------------------------------------
