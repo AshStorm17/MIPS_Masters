968ed3d2649502a268f968fec42eab282018f66c
Rohit Raj <102911571+rohitt-raj@users.noreply.github.com>
2024-11-01 19:19:52 +0530

    Update registers.py

--- components/registers.py
+++ components/registers.py
@@ -1,7 +1,21 @@
 # --------------------------------------------
 class Registers:
-    def __init__(self):
+    def __init__(self, initialise=False):
         self.reg = [""] * 32 # 32 registers
+
+        self.reg[0] = format(0, '032b')     # $zero register
+        self.reg[31] = format(0, '032b')    # $ra = 0 (default = exit main())
+        self.reg[29] = format(1024, '032b') # $sp = 4092 (default pointing to top address in the memory)
+        self.reg[30] = format(1024, '032b') # $fp = 4092 (default pointing to top address in the memory)
+
+        # uninitialised: global pointer, argument regs, value regs
+
+        if initialise==True:
+            for i in range(8,26):
+                self.reg[i] = "0"*32 # initialise s0-s7 and temporaries with 0's
+            self.reg[16] = format(5, '032b')    # $s0 = 5
+            self.reg[17] = format(10, '032b')   # $s1 = 10
+            self.reg[18] = format(1, '032b')    # $s2 = 1
 
     def write(self, number, value):
         self.reg[number] = value
@@ -20,5 +34,3 @@ class Registers:
     
     def __setitem__(self, key, value):
         self.reg[key] = value
-
-# --------------------------------------------
