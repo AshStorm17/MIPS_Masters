bbafd692c4cb4990de74d115483aebbb07d2e9fe
lucifer4073 <lucifer4073@gmail.com>
2024-11-01 18:43:30 +0530

    Memory parsing corrected

--- components/memory.py
+++ components/memory.py
@@ -7,7 +7,10 @@ class Memory:
         self.data = [""]*4*1024 # 1024 words
 
     def store(self, addr, value):
-        self.data[addr] = value
+        self.data[addr]=value[:8]
+        self.data[addr+1]=value[8:16]
+        self.data[addr+2]=value[16:24]
+        self.data[addr+3]=value[24:32]
 
     def load(self, addr):
         return self.data[addr]
