aa2c335402f22552b4939303a540cbfb786791df
lucifer4073 <lucifer4073@gmail.com>
2024-11-01 19:31:27 +0530

    stages.py modified

--- stages.py
+++ stages.py
@@ -8,7 +8,7 @@ class MIPSProcessor:
         self.memory = mem
         self.alu = alu
         self.registers = reg
-
+        self.pc=0
         self.IF_ID = {
             'PC': None,
             'instruction': None,
@@ -17,10 +17,10 @@ class MIPSProcessor:
 
         self.ID_EX = {
             'instruction': None, #of class type instruction
-            'valid': False,
-            'RD_1':None,
-            'RD_2':None,
-            'Immediate':None,
+            # 'valid': False,
+            # 'RD_1':None,
+            # 'RD_2':None,
+            # 'Immediate':None,
             'PC':None
         }
 
@@ -28,7 +28,19 @@ class MIPSProcessor:
     def fetch(self):
         pass
     def decode (self):
-        pass
+        inst=self.IF_ID['instruction']
+        opcode=int (inst[:6],2)
+        if opcode==0:
+            inst=Instruction(type=0,instruction=inst)
+        elif opcode in [2,3]:
+            inst=Instruction(type=2,instruction=inst)
+        else:
+            inst=Instruction(type=1,instruction=inst)
+        
+        self.ID_EX['instruction']=inst
+        self.ID_EX['PC']=self.pc
+        print(self.ID_EX)
+    
     def execute(self):
         pass
     def dmem(self):
@@ -39,13 +51,13 @@ class MIPSProcessor:
         pass
 
 if __name__=="__main__":
-    file_path="assets\\binary.txt"
+    file_path="assets\\binary_2.txt"
     # Step 1 ---------------------------------------
-    memory = Memory()
+    memory = Memory(initialise=True)
     alu = ALU()
-    registers = Registers()
+    registers = Registers(initialise=True)
     parse_mips_file(file_path,memory)
-    print(memory.data)
+    print(memory.data[:20])
     
 
     pass
